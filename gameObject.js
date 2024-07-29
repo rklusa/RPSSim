d5b69dc67c7c56a9d7d477bc518d651385ff95aa
rklusa <ryan.klusacek@hotmail.ca>
2024-07-29 16:29:17 -0400

    first commit

---
+++ gameObject.js
@@ -0,0 +1,69 @@
+class GameObject {
+    static width = 32;
+    static height = 32;
+    //static img = new Image();
+    //static string = '';
+
+    constructor(x, y, t){
+        this.x = x;
+        this.y = y;
+        this.speed = 2;
+        this.angle = 0;
+        this.t = t;
+        this.status = "empty";
+        
+    }
+
+    draw(ctx) {
+
+        switch(this.t) {
+            case 1:
+                this.string = '🧻';
+                break;
+            case 2:
+                this.string = '💎';
+                break;
+            case 3:
+                this.string = '✂️';
+                break;
+        }
+
+        ctx.fillText(this.string, this.x, this.y);
+    }
+
+    moveTowards(targetX, targetY) {
+        this.angle = Math.atan2(targetY - this.y, targetX - this.x);
+        this.x += (this.speed + 2) * Math.cos(this.angle);
+        this.y += (this.speed + 2) * Math.sin(this.angle);
+        this.avoidBorder();
+    }
+
+    moveAway(targetX, targetY) {
+        this.angle = Math.atan2(targetY - this.y, targetX - this.x);
+        this.x -= this.speed * Math.cos(this.angle);
+        this.y -= this.speed * Math.sin(this.angle);
+        this.avoidBorder();
+    }
+
+    //calcDistance(targetX, targetY) {
+        //return Math.sqrt((this.x - targetX) ** 2  + (this.y - targetY) ** 2);
+    //}
+
+    avoidBorder() {
+        if (this.x < 20 ) {
+            this.moveTowards(this.x + 20, this.y);
+        } else if (this.x > 800 - 20) {
+            this.moveTowards(this.x - 20, this.y);
+        }
+
+        if (this.y < 20) {
+            this.moveTowards(this.x, this.y + 20);
+        }
+        else if (this.y > 600 - 20) {
+            this.moveTowards(this.x, this.y - 20);
+        }
+
+
+    }
+}
+export {GameObject};
---
+++ main.js
@@ -0,0 +1,117 @@
+
+    import { GameObject } from "./gameObject.js";
+    
+    // paper(t=1) => rock(t=2) => scissors(t=3) =>
+    
+    const canvas = document.getElementById("mainCanvas");
+    const ctx = canvas.getContext("2d");
+    const deltaT = 30;
+    const numOfPlayers = 30;
+    const entities = [];
+    const detectionR = 100;
+    const conversionR = 30;
+
+    function spawnPlayers()
+    {
+        let n = numOfPlayers / 3;
+
+        for(let i = 0; i < n; i++) {
+            let tempObj = new GameObject(Math.random() * canvas.width, Math.random() * canvas.height, 1);
+            entities.push(tempObj);
+        }
+        for(let j = 0; j < n; j++) {
+            let tempObj = new GameObject(Math.random() * canvas.width, Math.random() * canvas.height, 2);
+            entities.push(tempObj);
+        }
+        for(let k = 0; k < n; k++) {
+            let tempObj = new GameObject(Math.random() * canvas.width, Math.random() * canvas.height, 3);
+            entities.push(tempObj);
+        }
+    }
+
+    function calcDistance(x1, y1, x2, y2) {
+        return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    }
+
+    function handleUpdate()
+    {
+        entities.forEach(function(e) {
+            let targets = [];
+            let currentTarget = undefined;
+            let d;
+            let low = Number.POSITIVE_INFINITY;
+            let ranX = Math.floor(Math.random() * canvas.width);
+            let ranY = Math.floor(Math.random() * canvas.height);
+
+            if (e.t == 1){
+                targets = entities.filter((i) => i.t == 2);
+            }
+            else if (e.t == 2) {
+                targets = entities.filter((i) => i.t == 3);
+            }
+            else if (e.t == 3) {
+                targets = entities.filter((i) => i.t == 1);
+            }
+
+            if (targets.length != 0) {
+                for (var i = targets.length-1; i>=0; i--) {
+                    d = calcDistance(e.x, e.y, targets[i].x, targets[i].y)
+
+                    if (d < low) {
+                        low = d;
+                    }
+                    if (d == low) {
+
+                       currentTarget = targets[i];
+                    }
+                }
+            } else {
+                currentTarget = undefined;
+            }
+
+            if (currentTarget != undefined) {
+                if (calcDistance(e.x, e.y, currentTarget.x, currentTarget.y) < detectionR) {
+                        e.moveTowards(currentTarget.x, currentTarget.y)
+                        e.status = "attacking";
+        
+                        if (calcDistance(e.x, e.y, currentTarget.x, currentTarget.y) < conversionR) {
+                            currentTarget.t = e.t;
+                        }
+                }
+                else {
+                    e.moveTowards(ranX, ranY);
+                    e.status = "wandering";
+                }
+            
+            } else {
+                e.moveTowards(ranX, ranY);
+                e.status = "wandering";
+            }
+
+            console.log(e.t);
+            //console.log(targets);
+            console.log(currentTarget);
+            console.log(e.status);
+            
+            e.draw(ctx);
+        });
+    }
+
+    
+
+
+    function render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = '16px serif'
+        ctx.textAlign = "center"; 
+        ctx.textBaseline = "middle"; 
+
+        handleUpdate();
+    }
+    spawnPlayers();
+    //render();
+    setInterval(render, deltaT);
+
+
+
+
